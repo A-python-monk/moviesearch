14e88dc6d3722b977821a973adfb8a42eb98204c
pythonmonk <shubhamraj9372@gmail.com>
2022-03-11 05:38:19 +0530

    done

--- src/components/card.js
+++ src/components/card.js
@@ -10,10 +10,10 @@ function MovieCard(props) {
         <div className="meracard d-flex  " style={{width: '65%'}}>
         <img src={imageApi + props.movie.poster_path} className="card-img-top p-2" alt={props.movie.title} style={{width: '10rem'}}/>
         <div className="card-body p2">
-            <h2>{props.movie.title} </h2>
-            <p>Release date : {props.movie.release_date} </p>
-            <p>Rating : {props.movie.vote_average} </p>
-            <p>{props.movie.overview} </p>
+            <h2 className='mytitle'>{props.movie.title} </h2>
+            <p className='mytitle'>Release date : {props.movie.release_date} </p>
+            <p className='mytitle'>Rating : {props.movie.vote_average} </p>
+            <p className='mytitle'>{props.movie.overview} </p>
         </div>
       </div>
       </div>
